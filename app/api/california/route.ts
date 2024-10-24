c08392962b0eda782c00ebdd3a81d1478f2e77fa
alexis <alexis.anzieu@gmail.com>
2024-10-24 14:53:01 -0400

    maybe 2

--- app/api/california/route.ts
+++ app/api/california/route.ts
@@ -9,6 +9,11 @@ export async function GET() {
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
       "Accept-Language": "en-US,en;q=0.5",
       Referer: "https://www.ssense.com/",
+      "Upgrade-Insecure-Requests": "1",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "same-origin",
+      "Sec-Fetch-User": "?1",
     },
   });
   const cookies = response.headers.get("set-cookie");
