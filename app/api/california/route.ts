0301f831cb23c601ae5db8e150aacf0a78fb445c
alexis <alexis.anzieu@gmail.com>
2024-10-24 13:43:32 -0400

    first commit

---
+++ app/api/california/route.ts
@@ -0,0 +1,22 @@
+export const preferredRegion = ["us-west-1"];
+export const dynamic = "force-dynamic"; // no caching
+
+export async function GET() {
+  const response = await fetch("https://www.ssense.com");
+  const cookies = response.headers.get("set-cookie");
+
+  const datePattern = /\d{1,2} \w{3} \d{4}/;
+
+  const parsedCookies = cookies
+    ?.split(",")
+    .map((cookie) => cookie.trim())
+    .filter((cookie) => !datePattern.test(cookie));
+
+  const jsonResponse = {
+    cookies: parsedCookies,
+  };
+
+  return new Response(JSON.stringify(jsonResponse), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
