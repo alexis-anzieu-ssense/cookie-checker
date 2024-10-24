6b9d74b2ec35b6317984a6a6536b085b09c3b52f
alexis <alexis.anzieu@gmail.com>
2024-10-24 14:49:27 -0400

    maybe

--- app/api/california/route.ts
+++ app/api/california/route.ts
@@ -1,10 +1,16 @@
-import { v4 as uuidv4 } from "uuid";
-
 export const dynamic = "force-dynamic"; // no caching
 
 export async function GET() {
-  const uuid = uuidv4();
-  const response = await fetch(`https://www.ssense.com/en-ca/men?${uuid}`);
+  const response = await fetch(`https://www.ssense.com/en-ca/men`, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
+      Referer: "https://www.ssense.com/",
+    },
+  });
   const cookies = response.headers.get("set-cookie");
 
   const datePattern = /\d{1,2} \w{3} \d{4}/;
